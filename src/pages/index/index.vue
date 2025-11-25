6f1a745c39d17cae787e76f4c15732f489d81f0d
liu-dejin <liudejin@kis2.cn>
2025-11-25 20:16:24 +0800

    feat: 轮播图渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
+import { getHomeBannerApi } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
-
-//
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem } from '@/types/home'
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerApi()
+  bannerList.value = res.result
+}
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <!-- 自定义导航条 -->
   <CustomNavbar />
   <!-- 自定义轮播图 -->
-  <XtxSwiper />
+  <XtxSwiper :list="bannerList" />
 </template>
 
 <style lang="scss">
